dcddaec58637892eb51fc52c3949dfc00fb0dbe8
kyungso <cks678928@gmail.com>
2019-09-16 23:17:47 +0900

    meta description update

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,13 +2,11 @@ import React from 'react';
 import Helmet from 'react-helmet';
 import styles from './Layout.module.scss';
 
-const Layout = ({ children, title, description }) => (
+const Layout = ({ children, title }) => (
   <div className={styles.layout}>
-    <Helmet>
+    <Helmet defaultTitle="Kingso\'s blog">
       <html lang="en" />
-      <title>{title}</title>
       <meta name="google-site-verification" content="mbgLz8ZMqCut4Jt1gFK1_ie6CSHvAN39Mijkn1EpPGc" />
-      <meta name="description" content={description} />
     </Helmet>
     {children}
   </div>
